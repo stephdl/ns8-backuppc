c627db9842f01fd0647ddf226019f645064b0103
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-05-27 09:26:14 +0200

    chore: Update build-images.sh to use backuppc:6.0.4 image
    This commit updates the build-images.sh script to use the docker.io/tiredofit/backuppc:6.0.4 image instead of the previous version 6.0.2. This ensures that the latest stable version of the BackupPC image is used for building the container.

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm cluster:accountconsumer" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/tiredofit/backuppc:6.0.2" \
+    --label="org.nethserver.images=docker.io/tiredofit/backuppc:6.0.4" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
